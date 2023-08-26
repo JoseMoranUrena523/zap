5fe2af483f70e8d9d6b8a7e391be2bfe10f8f6bc
Jose Moran Urena <jose@josemoranurena.tech>
2023-08-26 11:02:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,111 +1,40 @@
 const express = require('express');
-const session = require('express-session');
-const passport = require('passport');
-const Auth0Strategy = require('passport-auth0');
-const request = require('request');
-const cors = require('cors');
-const port = process.env.PORT || 3000;
+const { auth, requiresAuth } = require('express-openid-connect');
 
 const app = express();
 
-app.use(session({
-  secret: 'zap-incredible-session-secret-of-them-all',
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    secure: true,
-    sameSite: "strict"
-  }
-}));
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'zap-secretive-secret-that-no-one-knows-haha',
+  baseURL: 'https://goldfish-app-nusxd.ondigitalocean.app',
+  clientID: 'zrsJruo4diUxQLhJ2iBti3mBPZH1ioRn',
+  issuerBaseURL: 'https://zap-lightning.us.auth0.com'
+};
 
-app.use(cors());
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
 
-passport.use(new Auth0Strategy({
-    domain: 'zap-lightning.us.auth0.com',
-    clientID: 'zrsJruo4diUxQLhJ2iBti3mBPZH1ioRn',
-    clientSecret: 'ZkIZBNpxM4OaZc1eKAjFOajL55Al4RaGiFLK5NtDJNt_3w42wKaizCGQoN3WSOjM',
-    callbackURL: 'https://goldfish-app-nusxd.ondigitalocean.app/callback'
-}, (req, accessToken, refreshToken, extraParams, profile, done) => {
-    request.get(
-      {
-        url: 'https://zap-lightning.us.auth0.com/userinfo',
-        headers: {
-          Authorization: `Bearer ${accessToken}`
-        }
-      },
-      (err, response, body) => {
-        if (err) {
-          console.log('Error in Auth0Strategy:', err);
-          return done(err);
-        }
-  
-        try {
-          const userInfo = JSON.parse(body);
-          profile.user_info = userInfo;
-          console.log('Auth0 callback successful:', profile);
-          return done(null, profile);
-        } catch (error) {
-          console.log('Error parsing user info:', error);
-          return done(error);
-        }
-      }
-    );
-  }));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
+// Route for the root page '/'
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
-app.get('/login', passport.authenticate('auth0', {
-  scope: 'openid profile'
-}));
-
-app.get('/callback',
-  passport.authenticate('auth0', {
-    failureRedirect: '/'
-  }),
-  (req, res) => {
-    console.log('Successful authentication, redirecting to /dashboard');
+  if (req.oidc.isAuthenticated()) {
     res.redirect('/dashboard');
-  }
-);
-
-app.get('/dashboardData', (req, res) => {
-  try {
-    const userDisplayName = req.session.passport.user.displayName;
-    const userId = req.session.passport.user.user_id;
-
-    const userProfile = {
-      displayName: userDisplayName,
-      userId: userId,
-    };
-
-    res.status(200).json(userProfile);
-  } catch (error) {
-    res.status(500).json({ error: 'An error occurred while fetching dashboard data' });
+  } else {
+    res.sendFile(__dirname + '/index.html');
   }
 });
 
-app.get('/dashboard', (req, res) => {
+// Protected route for the profile page
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+
+// Route for the dashboard page
+app.get('/dashboard', requiresAuth(), (req, res) => {
   res.sendFile(__dirname + '/dashboard.html');
 });
 
-passport.serializeUser((user, done) => {
-  done(null, user);
-});
-  
-passport.deserializeUser((user, done) => {
-  done(null, user);
-});
-
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).json({ error: 'Something went wrong' });
-});
-
-app.listen(port, () => {
-  console.log(`Zap Lightning is up and listening on port ${port}!`);
+// Start server
+app.listen(8080, () => {
+  console.log('Server started on https://goldfish-app-nusxd.ondigitalocean.app/');
 });
