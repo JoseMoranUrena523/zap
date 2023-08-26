cd855046a48ca4714fd721ec40e3ff6c1ef669a6
Jose Moran Urena <jose@josemoranurena.tech>
2023-08-26 09:10:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -19,8 +19,6 @@ app.use(session({
   }
 }));
 
-app.use(csurf());
-
 app.use(helmet.contentSecurityPolicy({
   directives: {
     defaultSrc: ["'self'"],
@@ -80,12 +78,11 @@ app.get('/', (req, res) => {
 });
 
 // Auth Routes
-app.get('/login', csurf(), passport.authenticate('auth0', {
+app.get('/login', passport.authenticate('auth0', {
   scope: 'openid profile'
 }));
 
 app.get('/callback',
-  csurf(),
   passport.authenticate('auth0', {
     failureRedirect: '/'
   }),
