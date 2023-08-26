e1cfa91893e09a15f419618951636d35b99ec09a
Jose Moran Urena <jose@josemoranurena.tech>
2023-08-26 09:54:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const session = require('express-session');
 const passport = require('passport');
 const Auth0Strategy = require('passport-auth0');
 const request = require('request');
+const csurf = require('csurf');
 const helmet = require('helmet');
 
 const app = express();
 
 // Configure session
 app.use(session({
-  secret: 'zap-incredible-session-secret-of-them-all',
+  secret: '*',
   resave: false,
   saveUninitialized: true,
   cookie: {
@@ -18,36 +19,58 @@ app.use(session({
   }
 }));
 
-app.use(helmet());
+app.use(csurf());
+
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'", "'unsafe-inline'", "cdn.jsdelivr.net"],
+    styleSrc: ["'self'", "cdn.jsdelivr.net"],
+    imgSrc: ["'self'"],
+    objectSrc: ["'none'"]
+  }
+}));
+
+app.use(helmet.frameguard({ action: 'deny' })); // X-Frame-Options
+app.use(helmet.xssFilter()); // X-XSS-Protection
+app.use(helmet.hsts({ maxAge: 31536000, includeSubDomains: true, preload: true })); // Strict-Transport-Security
+
+// Debugging
+passport.debug = true;
 
 // Configure Passport
 passport.use(new Auth0Strategy({
-    domain: 'dev-zggqvh0tncla0n3k.us.auth0.com',
-    clientID: '7r0iximHfNEJRa3pJduqo8x8ak5LKRWT',
-    clientSecret: '8H24IaSP3zmRCIPoPmrlHKwPmT456lkDbrHb8I2ZDnAGYA4u7PXWMHbIRKe9goT-',
-    callbackURL: 'https://zap-lightning-6bpgo.ondigitalocean.app/callback'
-}, async (accessToken, refreshToken, extraParams, profile, done) => {
-    try {
-        const userInfoResponse = await fetch('https://dev-zggqvh0tncla0n3k.us.auth0.com/userinfo', {
-            headers: {
-                Authorization: `Bearer ${accessToken}`
-            }
-        });
-
-        if (!userInfoResponse.ok) {
-            console.error('Error in fetching user info:', userInfoResponse.statusText);
-            return done(new Error('Failed to fetch user info'));
+    domain: '*',
+    clientID: '*',
+    clientSecret: '*',
+    callbackURL: '*'
+}, (req, accessToken, refreshToken, extraParams, profile, done) => {
+    // Make a request to the /userinfo endpoint using the accessToken
+    request.get(
+      {
+        url: '*/userinfo', // Replace with your Auth0 domain
+        headers: {
+          Authorization: `Bearer ${accessToken}`
         }
-
-        const userInfo = await userInfoResponse.json();
-        profile.user_info = userInfo;
-        console.log('Auth0 callback successful:', profile);
-        return done(null, profile);
-    } catch (error) {
-        console.error('Error in Auth0Strategy:', error);
-        return done(error);
-    }
-}));
+      },
+      (err, response, body) => {
+        if (err) {
+          console.log('Error in Auth0Strategy:', err);
+          return done(err);
+        }
+  
+        try {
+          const userInfo = JSON.parse(body);
+          profile.user_info = userInfo;
+          console.log('Auth0 callback successful:', profile);
+          return done(null, profile);
+        } catch (error) {
+          console.log('Error parsing user info:', error);
+          return done(error);
+        }
+      }
+    );
+  }));
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -58,11 +81,12 @@ app.get('/', (req, res) => {
 });
 
 // Auth Routes
-app.get('/login', passport.authenticate('auth0', {
+app.get('/login', csurf(), passport.authenticate('auth0', {
   scope: 'openid profile'
 }));
 
 app.get('/callback',
+  csurf(),
   passport.authenticate('auth0', {
     failureRedirect: '/'
   }),
@@ -72,20 +96,42 @@ app.get('/callback',
   }
 );
 
-app.get('/dashboard', (req, res) => {
-  // Check if the user is authenticated
-  if (req.isAuthenticated()) {
-    // Render the dashboard
-    res.sendFile(__dirname + '/dashboard.html');
-  } else {
-    // Redirect to login if not authenticated
-    res.redirect('/login');
+app.get('/dashboardData', (req, res) => {
+  try {
+    // Access user info from the session
+    const userDisplayName = req.session.passport.user.displayName;
+    const userId = req.session.passport.user.user_id;
+
+    // Create the user profile JSON object
+    const userProfile = {
+      displayName: userDisplayName,
+      userId: userId,
+    };
+
+    res.status(200).json(userProfile);
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred while fetching dashboard data' });
   }
+});
+
+// Serve the dashboard.html file for the /dashboard route
+app.get('/dashboard', (req, res) => {
+  res.sendFile(__dirname + '/dashboard.html');
+});
+
+// Passport serialize function
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+  
+// Passport deserializeUser function
+passport.deserializeUser((user, done) => {
+  done(null, user);
 });
 
 // Error handling middleware
 app.use((err, req, res, next) => {
-  console.error('An error occurred:', err.message);
+  console.error(err); // Log the error for debugging purposes
 
   // Send an appropriate error response to the client
   res.status(500).json({ error: 'Something went wrong' });
@@ -93,5 +139,5 @@ app.use((err, req, res, next) => {
 
 // Start server
 app.listen(8080, () => {
-  console.log('Server started on https://zap-lightning-6bpgo.ondigitalocean.app/');
+  console.log('Server started on *');
 });
