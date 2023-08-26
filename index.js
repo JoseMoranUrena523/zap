3c82c31908d8bd3173c7130aec32e82086b44724
Jose Moran Urena <jose@josemoranurena.tech>
2023-08-26 09:58:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ const session = require('express-session');
 const passport = require('passport');
 const Auth0Strategy = require('passport-auth0');
 const request = require('request');
-const csurf = require('csurf');
-const helmet = require('helmet');
 
 const app = express();
 
 // Configure session
 app.use(session({
-  secret: '*',
+  secret: 'zap-incredible-session-secret-of-them-all',
   resave: false,
   saveUninitialized: true,
   cookie: {
@@ -19,36 +17,17 @@ app.use(session({
   }
 }));
 
-app.use(csurf());
-
-app.use(helmet.contentSecurityPolicy({
-  directives: {
-    defaultSrc: ["'self'"],
-    scriptSrc: ["'self'", "'unsafe-inline'", "cdn.jsdelivr.net"],
-    styleSrc: ["'self'", "cdn.jsdelivr.net"],
-    imgSrc: ["'self'"],
-    objectSrc: ["'none'"]
-  }
-}));
-
-app.use(helmet.frameguard({ action: 'deny' })); // X-Frame-Options
-app.use(helmet.xssFilter()); // X-XSS-Protection
-app.use(helmet.hsts({ maxAge: 31536000, includeSubDomains: true, preload: true })); // Strict-Transport-Security
-
-// Debugging
-passport.debug = true;
-
 // Configure Passport
 passport.use(new Auth0Strategy({
-    domain: '*',
-    clientID: '*',
-    clientSecret: '*',
-    callbackURL: '*'
+    domain: 'dev-zggqvh0tncla0n3k.us.auth0.com',
+    clientID: '7r0iximHfNEJRa3pJduqo8x8ak5LKRWT',
+    clientSecret: '8H24IaSP3zmRCIPoPmrlHKwPmT456lkDbrHb8I2ZDnAGYA4u7PXWMHbIRKe9goT-',
+    callbackURL: 'https://zap-lightning-6bpgo.ondigitalocean.app/callback'
 }, (req, accessToken, refreshToken, extraParams, profile, done) => {
     // Make a request to the /userinfo endpoint using the accessToken
     request.get(
       {
-        url: '*/userinfo', // Replace with your Auth0 domain
+        url: 'https://dev-zggqvh0tncla0n3k.us.auth0.com/userinfo', // Replace with your Auth0 domain
         headers: {
           Authorization: `Bearer ${accessToken}`
         }
@@ -81,12 +60,11 @@ app.get('/', (req, res) => {
 });
 
 // Auth Routes
-app.get('/login', csurf(), passport.authenticate('auth0', {
+app.get('/login', passport.authenticate('auth0', {
   scope: 'openid profile'
 }));
 
 app.get('/callback',
-  csurf(),
   passport.authenticate('auth0', {
     failureRedirect: '/'
   }),
@@ -139,5 +117,5 @@ app.use((err, req, res, next) => {
 
 // Start server
 app.listen(8080, () => {
-  console.log('Server started on *');
+  console.log('Server started on https://zap-lightning-6bpgo.ondigitalocean.app/');
 });
